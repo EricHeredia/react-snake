5f34617404e75ac1d1b86ae265ac8e9d7e6333de
EricHeredia <EricDavidHeredia@gmail.com>
2019-05-30 21:02:02 -0500

    adding snake component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react'
+import Snake from './components/snake'
+import './App.css'
 
 function App() {
+
+  const [snakeDots, setSnakeDots] = useState([
+    [0,0],
+    [2,0]
+  ])
+  
   return (
-    <div className="App">
+    <div className="game-area">
+      <Snake snakeDots={snakeDots} />
     </div>
-  );
+  )
 }
 
 export default App;
