e6cd0da269003fb3008faac804a1447127dd9a9e
EricHeredia <EricDavidHeredia@gmail.com>
2019-06-19 00:13:25 -0500

    started animation functions / will change later

--- src/components/DropMenu.js
+++ src/components/DropMenu.js
@@ -1,6 +1,19 @@
 import React from 'react'
 
-const DropMenu = () => {
+const DropMenu = (props) => {
+  /* 
+     Props will trigger a chain reaction of renders, slowly changing
+     styling to create a dropdown animation using react hooks
+  */
+
+  const animateDown = () => {
+    // Down animation here
+  }
+
+  const animateUp = () => {
+    // Up animation here
+  }
+
   return (
     <div>
       <p>DropMenu</p>
